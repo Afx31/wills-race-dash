7dac627d3bf327c190de4b095cad3c8a4951ec66
Afx31 <post4wjm@gmail.com>
2024-03-17 11:00:56 +1100

    Temp solution to test in car

--- client/index.js
+++ client/index.js
@@ -1,6 +1,69 @@
 var socket = io.connect('localhost:3000');
 
-socket.on('CANBusMessage', (data) => {  
+// NEW
+var latestData = null;
+var isDataPending = false;
+socket.on('CANBusMessage', (data) => {
+  latestData = data
+  if (!isDataPending) {
+    isDataPending = true;
+    requestAnimationFrame(updateUI)    ;
+  }
+});
+
+function updateUI() {
+  // Update UI elements here
+  if (latestData) {
+    // Get DOM elements
+    var rpmBar = document.getElementById('rpmbar');
+    var rpmNum = document.getElementById('rpmNum');
+    var speed = document.getElementById('speed');
+    var gear = document.getElementById('gear');
+    var voltage = document.getElementById('voltage');
+    var iat = document.getElementById('iat');
+    var ect = document.getElementById('ect');
+    var tpsBar = document.getElementById('tpsbar');
+    var tps = document.getElementById('tps');
+    var map = document.getElementById('map');
+    var lambdaRatio = document.getElementById('lambdaRatio');
+    var oilTemp = document.getElementById('oilTemp');
+    var oilPressure = document.getElementById('oilPressure');
+
+    // Update RPM bar width
+    rpmBar.style.width = ((latestData.rpm / 9000) * 100) + '%';
+
+    // Update other UI elements
+    rpmNum.textContent = latestData.rpm;
+    speed.textContent = latestData.speed;
+    gear.textContent = latestData.gear;
+    voltage.textContent = (latestData.voltage / 10).toFixed(1);
+    iat.textContent = latestData.iat;
+    ect.textContent = latestData.ect;
+    tpsBar.style.height = (latestData.tps / 655.35) + '%';
+    tps.textContent = latestData.tps;
+    map.textContent = (latestData.map / 20).toFixed(1);
+    lambdaRatio.textContent = (32768 / latestData.lambdaRatio).toFixed(2);
+    oilTemp.textContent = latestData.oilTemp;
+    oilPressure.textContent = latestData.oilPressure;
+
+    // Update RPM bar color
+    var percentInt = parseInt(rpmBar.style.width);
+    if (percentInt > 85)
+      rpmBar.style.backgroundColor = 'red';
+    else if (percentInt > 60)
+      rpmBar.style.backgroundColor = 'yellow';
+    else
+      rpmBar.style.backgroundColor = 'green';
+  }
+
+  // Reset pending flag
+  isDataPending = false;
+}
+
+
+
+// OLD
+/*socket.on('CANBusMessage', (data) => {  
   // if (data.changeDisplay === 1)
   //   window.location.href = 'http://localhost:3000/LapTimingDisplay';
 
@@ -56,4 +119,4 @@ socket.on('CANBusMessage', (data) => {
     rpmBar.style.setProperty('background-color', 'yellow', 'important');
   else
     rpmBar.style.setProperty('background-color', 'green', 'important');
-});
+});*/
